1c19d7fc7858811df7bee7b75ce86305c48cc30f
Rodolfo Bortolin <rodolfobortolin@gmail.com>
2017-02-17 13:33:22 -0200

    CDT-8 teste de comentário

--- bliki-core/src/main/java/com/ecore/atlassian/SummaryKeyApenderServlet.java
+++ bliki-core/src/main/java/com/ecore/atlassian/SummaryKeyApenderServlet.java
@@ -53,7 +53,7 @@ public class SummaryKeyApenderServlet extends HttpServlet {
 	//Relatorio 2
 	//Relatorio 3
 	
-	//opa
+	//opa 1
 
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
